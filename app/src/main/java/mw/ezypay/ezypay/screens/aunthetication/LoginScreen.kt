8c4d70db2bb05a95f6a35a983fbd0ff60354d540
giftthomu <giftthomu@gmail.com>
2024-06-26 15:40:49 +0200

    Update on the LoginScreen.kt

--- app/src/main/java/mw/ezypay/ezypay/screens/aunthetication/LoginScreen.kt
+++ app/src/main/java/mw/ezypay/ezypay/screens/aunthetication/LoginScreen.kt
@@ -1,7 +1,5 @@
 package mw.ezypay.ezypay.screens.aunthetication
 
-
-import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
